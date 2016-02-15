9fe07f4d4ac731131471cb457e4da6854d834fcc
eewano <eewano@gmail.com>
2016-02-15 21:52:09 +0900

    1.プレイヤーのタンクを作成。 2.ショットと行動範囲を作成。

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float FBSpeed;
+	[SerializeField] private float RotSpeed;
+	[SerializeField] private GameObject shot;
+	[SerializeField] private Transform shotSpawn;
+
+	public Boundary boundary;
+
+	void Start () {
+	
+	}
+
+	void Update()
+	{
+		float translation = Input.GetAxis ("Vertical") * FBSpeed;
+		float rotation = Input.GetAxis ("Horizontal") * RotSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+
+		transform.position = new Vector3(Mathf.Clamp (transform.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (transform.position.z, boundary.zMin, boundary.zMax));
+		
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+}
---
+++ Assets/ShotMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotMove : MonoBehaviour {
+
+	[SerializeField] private float shotSpeed;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = transform.forward * shotSpeed;
+	}
+}
